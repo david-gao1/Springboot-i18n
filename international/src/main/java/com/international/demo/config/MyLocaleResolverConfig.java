65d5147ea0e6a5dca6df33af4561083eb0a20725
v_liangggao <v_liangggao@email.com>
2021-01-28 10:39:44 +0800

    i18n 基础功能demo

---
+++ international/src/main/java/com/international/demo/config/MyLocaleResolverConfig.java
@@ -0,0 +1,14 @@
+package com.international.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+
+//加载这个bean
+@Configuration
+public class MyLocaleResolverConfig {
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new MyLocaleResolver();
+    }
+}
